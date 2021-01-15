46a04d20a8653f48af7c4c0f4b45b3e1ed2e0aa1
Romok1 <olaiyarommy@yahoo.com>
2021-01-15 21:35:16 +0100

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Romoke, welcome to devops class!'));
+app.get('/', (req, res) => res.send('Hello Romoke, you are done with aws lessons, congrats!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
